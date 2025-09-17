ce194845eb40d6db1be65fdf735c5e567594d2cf
purnima katta <142963455+purikatta@users.noreply.github.com>
2025-09-18 01:14:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    const hamburger =document.querySelector(".hamburger");
+    const menu_bar=document.querySelector(".navbar-menu .menu-bar");
+
+    hamburger.addEventListener("click",()=>{
+        menu_bar.classList.toggle("show");
+    });
+});
